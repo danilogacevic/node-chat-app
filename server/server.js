005bedb1fa2a52547a05cead07a69171dc540531
Danilo <dakarasadnik@gmail.com>
2017-12-10 21:44:25 +0100

    Emit new message on createMessage

--- server/server.js
+++ server/server.js
@@ -14,11 +14,7 @@ app.use(express.static(publicPath));
 io.on('connection',(socket) => {
 	console.log('New user connected');
 
-	socket.emit('newMessage',{
-		from: 'Jhon',
-		text: 'See you then',
-		createdAt:123123
-	})
+
 
 	socket.on('createEmail', (newEmail) => {
 		console.log('createEmail',newEmail)
@@ -26,6 +22,11 @@ io.on('connection',(socket) => {
 
 	socket.on('createMessage',(message) => {
 		console.log('createMessage',message);
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		})
 	});
 
 	socket.on('disconnect',() => {
